48e5c97f173ff3e7e680963ceace2d2da01c477b
yahya <elmokhtariyahya2001@gmail.com>
2026-09-08 13:38:41 +0100

    adapt fence position to new section spacing

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -205,7 +205,7 @@ export default function Hero() {
       </div>
       <div
         aria-hidden
-        className="pointer-events-none absolute left-0 -bottom-14 md:-bottom-10 z-10 w-40 md:w-56 xl:w-64 translate-y-1/2 md:translate-y-[70%] -translate-x-[12%] select-none"
+        className="pointer-events-none absolute left-0 -bottom-28 md:-bottom-24 z-10 w-40 md:w-56 xl:w-64 translate-y-1/2 md:translate-y-[70%] -translate-x-[12%] select-none"
         style={{ animation: "heroIconFloat 4.5s ease-in-out 0.8s infinite" }}
       >
         <img src="/assets/icons/hek.png" className="w-full" alt="" />
