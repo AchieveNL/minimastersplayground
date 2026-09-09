c15dd1a4e73db3b6ec9fbae378bd0a2e09d1d500
yahya <elmokhtariyahya2001@gmail.com>
2026-09-09 01:56:06 +0100

    lower fence slightly

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -179,7 +179,7 @@ export default function Hero({ ready = true }: { ready?: boolean }) {
       </div>
       <div
         aria-hidden
-        className="pointer-events-none absolute left-0 -bottom-10 md:-bottom-16 z-10 w-64 md:w-80 xl:w-96 translate-y-1/4 md:translate-y-1/3 -translate-x-[12%] select-none"
+        className="pointer-events-none absolute left-0 -bottom-10 md:-bottom-16 z-10 w-64 md:w-80 xl:w-96 translate-y-1/3 md:translate-y-[45%] -translate-x-[12%] select-none"
         style={{ animation: "heroIconFloat 4.5s ease-in-out 0.8s infinite" }}
       >
         <img src="/assets/icons/hek.png" className="w-full" alt="" />
